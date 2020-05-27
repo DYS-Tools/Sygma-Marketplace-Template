c80718a8757f4b3bc303a32aaf5b1e9a48f395f3
DurandSacha <sacha6623@gmail.com>
2020-05-27 15:18:52 +0200

    add category and relation to product

--- src/Entity/Product.php
+++ src/Entity/Product.php
@@ -27,6 +27,11 @@ class Product
      */
     private $description;
 
+    /**
+     * @ORM\ManyToOne(targetEntity=Category::class, inversedBy="product")
+     */
+    private $category;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -52,6 +57,18 @@ class Product
     public function setDescription(string $description): self
     {
         $this->description = $description;
+
+        return $this;
+    }
+
+    public function getCategory(): ?Category
+    {
+        return $this->category;
+    }
+
+    public function setCategory(?Category $category): self
+    {
+        $this->category = $category;
 
         return $this;
     }
