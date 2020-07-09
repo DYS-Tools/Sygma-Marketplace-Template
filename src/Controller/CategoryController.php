5a29e73820a7605ed2b457b596c7cec9329d10f0
DurandSacha <sacha6623@gmail.com>
2020-07-09 20:59:08 +0200

    remove category

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -3,7 +3,9 @@
 namespace App\Controller;
 
 use App\Entity\Category;
+use App\Entity\Product;
 use App\Form\NewCategoryFormType;
+use App\Repository\CategoryRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -24,8 +26,12 @@ class CategoryController extends AbstractController
         $categoryRepository = $this->getDoctrine()->getRepository(Category::class);
         $categories = $categoryRepository->findAll();
 
+        $productRepository = $this->getDoctrine()->getRepository(Product::class);
+        $products = $productRepository->findAll();
+
         return $this->render('dashboard/categoryHandler.html.twig', [
-            'categories' => $categories
+            'categories' => $categories,
+            'product' => $products
         ]);
     }
 
@@ -52,5 +58,19 @@ class CategoryController extends AbstractController
         return $this->render('category/new.html.twig', [
             'form' => $form->createView(),
         ]);
+    }
+
+    /**
+     * @Route("dashboard/category/delete/{id}", name="category_delete")
+     * @Security("is_granted('ROLE_ADMIN')")
+     */
+    public function delete_category(Request $request, Category $category, CategoryRepository $categoryRepo, $id)
+    {
+            $category = $categoryRepo->find($id);
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($category);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('category_handler');
     }
 }
