130d725ce5b8785a1300a956aa5668e6de39c845
DurandSacha <sacha6623@gmail.com>
2020-07-10 22:09:07 +0200

    remove Stripe Configuration

--- src/Service/payment.php
+++ src/Service/payment.php
@@ -29,36 +29,16 @@ class payment
 {
 
     private $entityManager;
-    private $secretStripeKeyTest;
-    private $publicStripeKeyTest;
     private $mailer;
     private $user;
 
-
-    public function __construct(EntityManagerInterface $em, $secretStripeKeyTest, $publicStripeKeyTest /* \Swift_Mailer $mailer*/)
+    public function __construct(EntityManagerInterface $em )
     {
         $this->em = $em;
-        $this->secretStripeKeyTest = $secretStripeKeyTest;
-        $this->publicStripeKeyTest = $publicStripeKeyTest;
-        /*$this->mailer = $mailer;*/
-    }
-
-    public function getStripeSecretCredentials(){
-        //return secret key
-        return $this->secretStripeKeyTest;
-    }
-
-    public function getStripePublicCredentials(){
-        //return public key
-        return $this->publicStripeKeyTest;
     }
 
     public function makePayment(Product $product, User $user )
     {
-        // Set your secret key. Remember to switch to your live secret key in production!
-        Stripe::setApiKey($this->secretStripeKeyTest);
-
-        
         //create Order
         $order = new Order;
         $order->setUser($user);
@@ -70,41 +50,13 @@ class payment
         $this->em->persist($order);
         $this->em->flush();
 
-        $price = $product->getPrice() * 100 ;
         
-        $path = rtrim(__DIR__, 'src\Service'); $path = $path . '\public\\'; $success = $path . '/sucesspayment'; $cancel = $path . '/cancelURL';
 
-        //TODO: Create success and cancel URL and redirect payment
-        $session = \Stripe\Checkout\Session::create([
-            'payment_method_types' => ['card'],
-            'line_items' => [[
-              'name' => $product->getName(),
-              'description' => $product->getDescription(),
-              //'images' => ['./' . $product->getImg1()],
-              'amount' => $price,
-              'currency' => 'eur',
-              'quantity' => 1,
-            ]],
-            'success_url' => 'https://sucessURL/'. $order->getId(),
-            'cancel_url' => 'https://CancelURL',
-          ]);
-
-            //$endpoint = 'whsec_8PVGe96UgcQBcS0lWUnfZKnJtalr1Fnx';
-
-          // $order->setStatus('Finished');
-        return $session;
+          // TODO: $order->setStatus('Finished');
+        return ;
     }
 
-    public function getConnectAccount(User $user ){
-        // for register a acct_XXXXX ( stripe_user_id variable ) in database
-
-        $client = HttpClient::create();
-        $response = $client->request('GET', 'https://dashboard.stripe.com/express/oauth/authorize?response_type=code&client_id=ca_HYxhtqNA8QfeeDMrU0lvmTIvBbDap4y9&scope=read_write');
-        
-        $this->em->flush();
-        return $response ;
     
-    }
 
     public function payout(){
         // transfet funds in acct_XXXXX account
