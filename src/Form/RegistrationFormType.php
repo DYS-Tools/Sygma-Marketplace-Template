a5bf8e6b2136f316d98134b479ae3aa2567b988e
DurandSacha <sacha6623@gmail.com>
2020-07-11 17:58:58 +0200

    add name on User entity

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -7,6 +7,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
@@ -54,7 +55,10 @@ class RegistrationFormType extends AbstractType
                 'required' => true,
                 'first_options'  => ['label' => 'Mots de passe'],
                 'second_options' => ['label' => 'Mots de passe ( sécurité )'],
-            ]);
+            ])
+            ->add('pseudo', TextType::class, [
+                'label' => 'votre pseudo'
+            ]);;
         ;
     }
 
