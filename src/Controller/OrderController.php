5e8411a6432a7e3d8f44c70ad4784262587a344e
Yohann76 <yohanndurand76@gmail.com>
2020-07-13 23:00:28 +0200

    [Payout] return valid json with create_order

--- src/Controller/OrderController.php
+++ src/Controller/OrderController.php
@@ -5,7 +5,8 @@ namespace App\Controller;
 use App\Entity\Order;
 use App\Entity\Product;
 use App\Service\payment;
-use http\Env\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpClient\CurlHttpClient;
 use Symfony\Component\Routing\Annotation\Route;
@@ -41,7 +42,7 @@ class OrderController extends AbstractController
         //dd($product);
         dump($session);
 
-        return $this->render('order/order.html.twig', [
+        return $this->render('order/choosePayment.html.twig', [
             'product' => $product
         ]);
     }
@@ -61,7 +62,7 @@ class OrderController extends AbstractController
         
         dd($session);
         
-        return $this->render('order/order.html.twig', [
+        return $this->render('order/choosePayment.html.twig', [
             'product' => $product
         ]);
     }
@@ -119,13 +120,16 @@ class OrderController extends AbstractController
      */
     public function createOrderWithPaypal(payment $payment, $product)
     {
+        // get product
         $productRepository = $this->getDoctrine()->getRepository(Product::class);
         $product = $productRepository->findOneBy(['id' => $product]);
+
+        // get current user
         $user = $this->getUser();
 
-        $session = $payment->makePayment($product, $user);
+        //$session = $payment->makePayment($product, $user);
 
-        return $this->render('order/order.html.twig', [
+        return $this->render('order/choosePayment.html.twig', [
             'product' => $product
         ]);
     }
@@ -141,8 +145,11 @@ class OrderController extends AbstractController
         // https://developer.paypal.com/docs/platforms/checkout/set-up-payments/
         $BearerAccessToken = $payment->connectPaypal();
         $accessToken= $BearerAccessToken;
+        dump($accessToken);
 
         $ch = curl_init('https://api.sandbox.paypal.com/v2/checkout/orders');
+
+        curl_setopt($ch, CURLOPT_POST, 1);
 
         // HEADER
         curl_setopt($ch, CURLOPT_HEADER, false);
@@ -164,10 +171,14 @@ class OrderController extends AbstractController
               "amount" :{
                 "currency_code" : "EUR",
                 "value" : "7.47"
+              },
+              "payee" : {
+                "email_address" : "yohanndurand76@gmail.com"
               }
             }
           ]
         }';
+
         curl_setopt($ch, CURLOPT_POSTFIELDS, $payloadData); // send JSON
 
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); // return caracter
@@ -181,12 +192,28 @@ class OrderController extends AbstractController
         $orderId = $resultJson->id ;  // return just OrderId
         dump($orderId);
 
-       // return $orderId ;
-        return $this->render('order/order.html.twig', [
-            'orderId' => $orderId
+       //return $orderId ;
+
+        /*
+        return new Response(
+            $orderId
+        );
+        */
+        
+        //return json Response
+        return new JsonResponse(
+            $resultJson
+        );
+
+
+        /*
+         return $this->render('order/choosePayment.html.twig', [
+            //'orderId' => $orderId
         ]);
+        */
+
         // curl debug
-        //$err = curl_error($ch); // dump($err) ;
+        // $err = curl_error($ch); // dump($err) ;
         //$httpStatusCode = curl_getinfo($ch, CURLINFO_HTTP_CODE); // dd($httpStatusCode);
     }
 
@@ -206,18 +233,23 @@ class OrderController extends AbstractController
         curl -v -k -X POST https://api.paypal.com/v2/checkout/orders/5O190127TN364715T/capture \
          -H 'PayPal-Request-Id: 7b92603e-77ed-4896-8e78-5dea2050476a' \
          -H 'PayPal-Partner-Attribution-Id:  BN-Code' \
-         -H 'Authorization: Bearer Access-Token' \
-         -H 'Content-Type: application/json' \
+         -H 'Authorization: Bearer Access-Token' \  Good
+         -H 'Content-Type: application/json' \   Good
          -d '{}'
         */
 
+        // todo : valid request .. ?
         $BearerAccessToken = $payment->connectPaypal();
         $accessToken = $BearerAccessToken;
+        dump($accessToken);
 
         // test get id order
         //$orderToken = $this->create_order();
 
-        $ch = curl_init('https://api.paypal.com/v2/checkout/orders/'.$orderId.'"/capture');
+        // $ch = curl_init('https://api.paypal.com/v2/checkout/orders/'.$orderId.'/capture'); // docs
+        $ch = curl_init('https://api.sandbox.paypal.com/v2/checkout/orders/'.$orderId.'/capture');  // sendbox api
+
+        curl_setopt($ch, CURLOPT_POST, 1);
 
         // HEADER
         curl_setopt($ch, CURLOPT_HEADER, false);
@@ -232,6 +264,7 @@ class OrderController extends AbstractController
         curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE);
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
 
+
         $payloadData = '
         {
         }';
@@ -241,11 +274,23 @@ class OrderController extends AbstractController
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); // return caracter
 
         $result = curl_exec($ch);
+        dump($result);  // no json
+
+        // get error
+        $err = curl_error($ch);
+        dump($err) ;
+
+
         curl_close($ch);
 
-        $resultJson = json_decode($result);
 
-        dd($resultJson);
+
+
+
+        $resultJson = json_decode($result);
+        dd($resultJson);  // json
+
+
         return $resultJson ;
     }
 
