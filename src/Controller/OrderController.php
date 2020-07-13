cda104a597fe3b50e03bda0e6caee6867d752e78
Yohann76 <yohanndurand76@gmail.com>
2020-07-13 18:49:42 +0200

    [Payout] add simple paypal button in js

--- src/Controller/OrderController.php
+++ src/Controller/OrderController.php
@@ -244,7 +244,7 @@ class OrderController extends AbstractController
         curl_close($ch);
 
         $resultJson = json_decode($result);
-        
+
         dd($resultJson);
         return $resultJson ;
     }
