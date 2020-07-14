b5fcd06c58c8c829d2850ff0f59e82cfd3b9c43c
Yohann76 <yohanndurand76@gmail.com>
2020-07-15 00:04:58 +0200

    [Payout] add errorPayment and succesPayment

--- src/Controller/OrderController.php
+++ src/Controller/OrderController.php
@@ -5,6 +5,8 @@ namespace App\Controller;
 use App\Entity\Order;
 use App\Entity\Product;
 use App\Service\payment;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -19,99 +21,11 @@ class OrderController extends AbstractController
 
     private $client;
 
-    public function __construct(HttpClientInterface $client )
+    public function __construct(HttpClientInterface $client, EntityManagerInterface $em )
     {
         $this->client = $client;
+        $this->em = $em;
     }
-
-    //////////////////////////////////////////////////////
-    /* Sacha Dev test */
-    //////////////////////////////////////////////////////
-
-    /**
-     * @Route("/order/{product}", name="order_test")
-     * @Security("is_granted('ROLE_USER')")
-     */
-    public function initOrder(payment $payment, $product)
-    {
-        $productRepository = $this->getDoctrine()->getRepository(Product::class);
-        $product = $productRepository->findOneBy(['id' => $product]);
-        $user = $this->getUser();
-        //dd($user);
-        $session = $payment->makePayment($product, $user);
-        //dd($product);
-        //dump($session);
-
-        return $this->render('order/choosePayment.html.twig', [
-            'product' => $product
-        ]);
-    }
-
-    /**
-     * @Route("/redirectPayment/{product}", name="redirectPayment")
-     * @Security("is_granted('ROLE_USER')")
-     */
-    public function redirectPayment(payment $payment, $product)
-    {
-        $user = $this->getUser();
-
-        $productRepository = $this->getDoctrine()->getRepository(Product::class);
-        $product = $productRepository->findOneBy(['id' => $product]);
-
-        $session = $payment->makePayment($product, $user);
-        
-        dd($session);
-        
-        return $this->render('order/choosePayment.html.twig', [
-            'product' => $product
-        ]);
-    }
-
-    /**
-     * @Route("/successPayment/{orderId}", name="successPayment")
-     * @Security("is_granted('ROLE_USER')")
-     */
-    public function successPayment(payment $payment, $orderId, \Swift_Mailer $mailer)
-    {
-        $orderRepository = $this->getDoctrine()->getRepository(Order::class);
-        $order = $orderRepository->findOneBy(['id' => $orderId]);
-        $order->setStatus('Finished');
-
-            // \Swift_Mailer $mailer
-            $message = (new \Swift_Message('Web-Item-Market'))
-                ->setFrom('sacha6623@gmail.com')
-                ->setTo($this->getUser()->getEmail())
-                ->setBody(
-                    $this->renderView(
-                        'Emails/successPayment.html.twig',
-                        []),
-                 'text/html');
-            $mailer->send($message);
-
-            $this->addFlash('success', "Email has been send");
-            
-
-        // TODO: Validation de la commande 
-        // Incrementer la vente dans l'objet product
-        // incrementer $order->getAmount() * 0.80;      // 80 % a availablePayout
-
-        return $this->render('order/success.html.twig', [
-        ]);
-    }
-
-    /**
-     * @Route("/cancelPayment", name="cancelPayment")
-     */
-    public function cancelPayment(payment $payment, $product)
-    {
-        //dd('redirection Stripe Here');
-        return $this->render('order/cancel.html.twig', [
-        ]);
-    }
-
-    //////////////////////////////////////////////////////
-    /* Yohann Dev test */
-    //////////////////////////////////////////////////////
 
     /**
      * @Route("/order/{product}", name="order")
@@ -127,10 +41,71 @@ class OrderController extends AbstractController
         // get current user
         $user = $this->getUser();
 
-        //$session = $payment->makePayment($product, $user);
+        // Create Order
+        $order = new Order;
+        $order->setUser($user);
+        $order->setAmount($product->getPrice());
+        $order->setStatus('waiting for payment');
+        $order->setCreated( new \DateTime() );
+        $order->setProduct($product);
+
+        $this->em->persist($order);
+        $this->em->flush();
+
+        // TODO: passer la commande dans la vue, pour ensuite injecter l'id sur le lien ?
+        // Todo : mettre en finish l'etat de la commande
 
         return $this->render('order/choosePayment.html.twig', [
             'product' => $product
+        ]);
+    }
+
+    /**
+     * @Route("/successPayment", name="successPayment")
+     * @Security("is_granted('ROLE_USER')")
+     * if payement succes
+     */
+    public function successPayment(payment $payment, \Swift_Mailer $mailer)
+    {
+        /*
+         * avec $orderId en argument et dans le lien
+
+        $orderRepository = $this->getDoctrine()->getRepository(Order::class);
+        $order = $orderRepository->findOneBy(['id' => $orderId]);
+        $order->setStatus('Finish');
+         */
+
+        // \Swift_Mailer $mailer
+        $message = (new \Swift_Message('Web-Item-Market'))
+            ->setFrom('sacha6623@gmail.com')
+            ->setTo($this->getUser()->getEmail())
+            ->setBody(
+                $this->renderView(
+                    'Emails/successPayment.html.twig',
+                    []),
+                'text/html');
+        $mailer->send($message);
+
+        $this->addFlash('success', "Email has been send");
+
+
+        // TODO: Validation de la commande
+
+        // Incrementer la vente dans l'objet product  // product + 1
+        // incrementer $order->getAmount() * 0.80;      // 80 % a availablePayout
+
+        return $this->render('order/successPayment.html.twig', [
+        ]);
+    }
+
+
+    /**
+     * @Route("/errorPayment", name="errorPayment")
+     */
+    public function errorPayment(payment $payment, $product)
+    {
+        // return view error..
+        return $this->render('order/errorPayment.html.twig', [
         ]);
     }
 
@@ -143,19 +118,14 @@ class OrderController extends AbstractController
     {
         // test function : http://localhost/perso/Web-Item-Market/public/createOrder
         // https://developer.paypal.com/docs/platforms/checkout/set-up-payments/
-
         // sendbox paypal
         // sb-lz8752580455@personal.example.com
         // d]s^zA<3
-
         $BearerAccessToken = $payment->connectPaypal();
         $accessToken= $BearerAccessToken;
         dump($accessToken);
-
         $ch = curl_init('https://api.sandbox.paypal.com/v2/checkout/orders');
-
         curl_setopt($ch, CURLOPT_POST, 1);
-
         // HEADER
         curl_setopt($ch, CURLOPT_HEADER, false);
         curl_setopt($ch, CURLOPT_HTTPHEADER, array(
@@ -163,11 +133,12 @@ class OrderController extends AbstractController
             'Accept: application/json',
             'Content-Type: application/json'
         ));
-
         // SSL certificat
         curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE);
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
 
+
+        // Todo : valid data product in json ? passer dans l'url de cette methode un productId qui va sur le template
         $payloadData = '
         {
           "intent" : "CAPTURE",
@@ -182,37 +153,17 @@ class OrderController extends AbstractController
         }';
 
         curl_setopt($ch, CURLOPT_POSTFIELDS, $payloadData); // send JSON
-
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); // return caracter
-
         $result = curl_exec($ch);
         curl_close($ch);
-
         $resultJson = json_decode($result);  // return complete json
         dump($resultJson);
-
         $orderId = $resultJson->id ;  // return just OrderId
         dump($orderId);
-
-       //return $orderId ;
-
-        /*
-        return new Response(
-            $orderId
-        );
-        */
-
         //return json Response
         return new JsonResponse(
             $resultJson
         );
-
-
-        /*
-         return $this->render('order/choosePayment.html.twig', [
-            //'orderId' => $orderId
-        ]);
-        */
 
         // curl debug
         // $err = curl_error($ch); // dump($err) ;
@@ -230,24 +181,13 @@ class OrderController extends AbstractController
         // test function : http://localhost/perso/Web-Item-Market/public/captureOrder
         // https://developer.paypal.com/docs/platforms/checkout/set-up-payments/
 
-        /*
-        curl -v -k -X POST https://api.paypal.com/v2/checkout/orders/5O190127TN364715T/capture \
-         -H 'PayPal-Request-Id: 7b92603e-77ed-4896-8e78-5dea2050476a' \
-         -H 'PayPal-Partner-Attribution-Id:  BN-Code' \
-         -H 'Authorization: Bearer Access-Token' \  Good
-         -H 'Content-Type: application/json' \   Good
-         -d '{}'
-        */
-
         $BearerAccessToken = $payment->connectPaypal();
         $accessToken = $BearerAccessToken;
         dump($accessToken);
 
         //$ch = curl_init('https://api.paypal.com/v2/checkout/orders/'.$orderId.'/capture'); // docs
         $ch = curl_init('https://api.sandbox.paypal.com/v2/checkout/orders/'.$orderId.'/capture');  // sendbox api
-
         curl_setopt($ch, CURLOPT_POST, 1);
-
         // HEADER
         curl_setopt($ch, CURLOPT_HEADER, false);
         curl_setopt($ch, CURLOPT_HTTPHEADER, array(
@@ -266,22 +206,17 @@ class OrderController extends AbstractController
         }';
 
         curl_setopt($ch, CURLOPT_POSTFIELDS, $payloadData); // send JSON
-
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); // return caracter
 
         $result = curl_exec($ch);
         dump($result);  // no json
 
         // get error
-        $err = curl_error($ch);
-        dump($err) ;
-
+        // $err = curl_error($ch); // dump($err) ;
         curl_close($ch);
 
-
-        // return json ?
         $resultJson = json_decode($result);
-        dd($resultJson);  // json
+        dd($resultJson);  // json   Error ?
 
         return $resultJson ;
     }
